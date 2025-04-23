28d5141c5561c7f9386d05207decc9e9c64287c3
anaesparapani <anaclaraesparapani@gmail.com>
2025-04-23 09:05:42 -0300

    OK

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2"); //Importa o mysql
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "10.89.240.91",
+  host: "10.89.240.81",
   user: "alunods",
   password: "senai@604",
   database: "vio_ana",
