5700349fb3ceb58d5e36260976105e56fb268edf
anaesparapani <anaclaraesparapani@gmail.com>
2025-06-02 11:28:40 -0300

    02-06

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userController = require("../controllers/userController");
-const verifyJWT = require('../services/verifyJWT');
+const verifyJWT = require("../services/verifyJWT");
+
+const compraController = require("../controllers/compraController");
 
 router.post("/user", userController.createUser);
 router.get("/user", verifyJWT, userController.getAllUsers);
@@ -16,24 +18,27 @@ router.get("/organizador", organizadorController.getAllOrganizador);
 router.put("/organizador", organizadorController.updateOrganizador);
 router.delete("/organizador/:id", organizadorController.deleteOrganizador);
 
-const eventoController = require('../controllers/eventoController');
+const eventoController = require("../controllers/eventoController");
 
 //rotas eventoController
-router.post('/evento', eventoController.createEvento);
-router.get('/evento', verifyJWT,eventoController.getAllEventos);
-router.put('/evento', eventoController.updateEvento);
-router.delete('/evento/:id', eventoController.deleteEvento);
-router.get('/evento/data', verifyJWT,eventoController.getEventosPorData);
-router.get('/evento/proximo', verifyJWT,eventoController.getEventosdia);
+router.post("/evento", eventoController.createEvento);
+router.get("/evento", verifyJWT, eventoController.getAllEventos);
+router.put("/evento", eventoController.updateEvento);
+router.delete("/evento/:id", eventoController.deleteEvento);
+router.get("/evento/data", verifyJWT, eventoController.getEventosPorData);
+router.get("/evento/proximo", verifyJWT, eventoController.getEventosdia);
 
 const ingressoController = require("../controllers/ingressoController");
 
 //rotas ingressoController
-router.post('/ingresso', ingressoController.createIngresso);
-router.get('/ingresso/evento/:id', ingressoController.getByIdEvento);
-router.get('/ingresso', ingressoController.getAllIngresos);
-router.put('/ingresso', ingressoController.updateIngresso);
-router.delete('/ingresso/:id', ingressoController.deleteIngresso);
+router.post("/ingresso", ingressoController.createIngresso);
+router.get("/ingresso/evento/:id", ingressoController.getByIdEvento);
+router.get("/ingresso", ingressoController.getAllIngresos);
+router.put("/ingresso", ingressoController.updateIngresso);
+router.delete("/ingresso/:id", ingressoController.deleteIngresso);
 
+// compraController
+router.post("/comprasimples", compraController.registrarCompraSimples);
+router.post("/compra", compraController.registrarCompra);
 
 module.exports = router;
